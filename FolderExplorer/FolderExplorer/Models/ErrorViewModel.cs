cbd25aac7cd2b46791ac3c8600fb5a795b3d1c28
Ihor Arkhypenko <wasbornindecember@gmail.com>
2023-11-23 21:11:29 +0200

    Initial commit. MVC project created

---
+++ FolderExplorer/FolderExplorer/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace FolderExplorer.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
